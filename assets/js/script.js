25fb6de120615b15b85fda0678fa59eda9b6f9d4
Peter Park <peterjpark@pm.me>
2023-04-17 18:52:02 -0400

    Add artist search box + ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,35 @@
-function searchSong(){
+var lyrics;
+var artist;
+
+function searchSong() {
   //let songLyrics = $("idname").value;
   //let songArtist = $("idname").value;
   $.ajax({
     type: "GET",
     data: {
-        apikey:"8aeb7ff0f51f21a364a803d7a9db035f",
-        q_lyrics: "thought i ran into you", //switch to songLyrics variable,
-        q_artist: "green day", //switch to songArtist variable,
-        //f_music_genre_id: "20",
-        format:"jsonp",
-        callback:"jsonp_callback"
+      apikey: "8aeb7ff0f51f21a364a803d7a9db035f",
+      q_lyrics: "thought i ran into you", //switch to songLyrics variable,
+      q_artist: "green day", //switch to songArtist variable,
+      //f_music_genre_id: "20",
+      format: "jsonp",
+      callback: "jsonp_callback",
     },
     url: "http://api.musixmatch.com/ws/1.1/track.search?page_size=10&page=1&s_track_rating=desc",
     dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
-    success: function(data) {
-        console.log(data); 
-        console.log(data.message.body.track_list[0].track.track_name);
+    jsonpCallback: "jsonp_callback",
+    contentType: "application/json",
+    success: function (data) {
+      console.log(data);
+      console.log(data.message.body.track_list[0].track.track_name);
     },
-    error: function(jqXHR, textStatus, errorThrown) {
-       console.log(jqXHR);
-       console.log(textStatus);
-       console.log(errorThrown);
-    }    
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    },
   });
- };
- //searchSong();
-
-
+}
+//searchSong();
 
 function querySpotify(searchParams) {
   // Used to get access token from Spotify API.
@@ -57,24 +58,21 @@ function querySpotify(searchParams) {
         },
       })
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => console.log(data));
     });
 }
 
 // Example function call. -- Searching for a track and artist.
-querySpotify("track:The%20Real%20Slim%20Shady%20artist:Eminem");
+querySpotify("track:Playing%20God%20artist:Polyphia");
 
-// Returns song information, preview, etc.
+$(function () {
+  var searchResultsEl = $("#search-results");
+  var resultCardsEl = $("#results-card");
 
-$(function (){
-
-  var searchResultsEl = $('#search-results');
-  var resultCardsEl = $('#results-card')
-
-
-
-
-
-
-
+  // Get user input for lyrics + artist.
+  $("#search-form").on("submit", function (e) {
+    e.preventDefault();
+    lyrics = $("#lyrics-input").val();
+    artist = $("#artist-input").val();
+  });
 });
