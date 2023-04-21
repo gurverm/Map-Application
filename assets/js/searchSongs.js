5f4e02f18f34f7bc375636342894a98ea39b10c8
Marek Polak <markalexpolak@gmail.com>
2023-04-20 21:07:25 -0400

    updated recentSongs function

--- assets/js/searchSongs.js
+++ assets/js/searchSongs.js
@@ -1,21 +1,19 @@
 function recentSongs() {
     const lyricsValue = $('#search-lyrics').val();
     const artistValue = $('#search-artist').val();
-    let searchHistory = [];
     const searchInfo = lyricsValue + ", " + artistValue;
-    console.log(localStorage.getItem('search'));
+    const storedVal = JSON.parse(localStorage.getItem('search'));
+    let searchHistory = [];
   
     // Retrieve the search history from local storage, or create an empty array if none exist
-    if (localStorage.getItem('search') == null){
+    if (storedVal == null){
         searchHistory = [searchInfo];
         localStorage.setItem('search', JSON.stringify(searchHistory));
         console.log("does this 1");
         $("#0").text(searchHistory[0]);
     }
-    else {
-        searchHistory = JSON.parse(localStorage.getItem('search'));
-        console.log(searchHistory);
-  
+    else if (!storedVal.includes(searchInfo)){
+        searchHistory = storedVal;
         // Add the searched info object to the search history array
         searchHistory.unshift(searchInfo);
         // Store the updated search history array in local storage
@@ -27,16 +25,16 @@ function recentSongs() {
   
         // Create a new button element with the search label
         for (let i = 0; i < searchHistory.length; i++){
-            let newSearch = $('#' + String(i));
+            const newSearch = $('#' + String(i));
             newSearch.text(searchHistory[i]);
         }
     }
 }
 
 function displaySongs(){
-    searchHistory = JSON.parse(localStorage.getItem('search'));
+    const searchHistory = JSON.parse(localStorage.getItem('search'));
     for (let i = 0; i < searchHistory.length; i++){
-        let newSearch = $('#' + String(i));
+        const newSearch = $('#' + String(i));
         newSearch.text(searchHistory[i]);
     }
 }
